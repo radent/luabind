9e9d880709bd0975de7f9fcfb9a4aaff11afe4ac
Yang Shan <yang@radiant-entertainment.com>
2015-09-23 13:26:16 -0700

    Adding no except(false) to destructor of call proxy

--- luabind/detail/object.hpp
+++ luabind/detail/object.hpp
@@ -1118,7 +1118,7 @@ namespace adl
           other.value_wrapper = 0;
       }
 
-      ~call_proxy()
+      ~call_proxy() noexcept(false)
       {
           if (value_wrapper)
               call((detail::null_type*)0);
