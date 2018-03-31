1ba1d9bc5e3a30c5869873d060167f4da3f9f35d
Gabriel Martinez <gmartinez@riotgames.com>
2018-03-31 14:38:48 -0700

    Initial changes to support macos

--- Makefile
+++ Makefile
@@ -8,10 +8,15 @@ endif
 
 .PHONY: default
 default:
-	$(MSBUILD_SERIAL) solutions/luabind.sln -p:configuration=debug,platform=$(MSBUILD_PLATFORM)
-	$(MSBUILD_SERIAL) solutions/luabind.sln -p:configuration=release,platform=$(MSBUILD_PLATFORM)
+	mkdir -p build
+	cd build && cmake -G $(RADIANT_CMAKE_GENERATOR) -DBOOST_ROOT=$(RADIANT_BOOST_ROOT) -DLUA_INCLUDE_DIR=../../lua/package/lua-5.1.5-coco/src/ -DLUA_LIBRARIES=lua -DLUA_LIBRARY_DIR=../../lua/package/lua-5.1.5-coco/src/ -DBUILD_TESTING=false ..
+	#cd build && cmake -G $(RADIANT_CMAKE_GENERATOR) -DBOOST_ROOT=$(RADIANT_BOOST_ROOT) -DLUA_DIR=../../lua/package/lua-5.1.5-coco/ ..
+	cd build && cmake --build .
+	#$(MSBUILD_SERIAL) solutions/luabind.sln -p:configuration=debug,platform=$(MSBUILD_PLATFORM)
+	#$(MSBUILD_SERIAL) solutions/luabind.sln -p:configuration=release,platform=$(MSBUILD_PLATFORM)
 
 .PHONY: clean
 clean:
 	-rm -rf solutions/debug
 	-rm -rf solutions/release
+	-rm -rf build
--- cmake/LuabindConfig.cmake
+++ cmake/LuabindConfig.cmake
@@ -1,14 +1,20 @@
 SET(Luabind_ROOT "${RADIANT_MODULE_ROOT}/luabind")
 SET(Luabind_INCLUDE_DIRS "${Luabind_ROOT}")
 
-IF("${RADIANT_BUILD_PLATFORM}" STREQUAL "x86")
-   SET(Luabind_LIBRARIES
-       optimized ${Luabind_ROOT}/solutions/Release/luabind.lib
-       debug     ${Luabind_ROOT}/solutions/Debug/luabind.lib)
-ELSE()
-   SET(Luabind_LIBRARIES
-       optimized ${Luabind_ROOT}/solutions/${RADIANT_BUILD_PLATFORM}/Release/luabind.lib
-       debug     ${Luabind_ROOT}/solutions/${RADIANT_BUILD_PLATFORM}/Debug/luabind.lib)
+IF(WIN32)
+    IF("${RADIANT_BUILD_PLATFORM}" STREQUAL "x86")
+        SET(Luabind_LIBRARIES
+            optimized ${Luabind_ROOT}/solutions/Release/luabind.lib
+            debug     ${Luabind_ROOT}/solutions/Debug/luabind.lib)
+    ELSE()
+        SET(Luabind_LIBRARIES
+            optimized ${Luabind_ROOT}/solutions/${RADIANT_BUILD_PLATFORM}/Release/luabind.lib
+            debug     ${Luabind_ROOT}/solutions/${RADIANT_BUILD_PLATFORM}/Debug/luabind.lib)
+    ENDIF()
+ELSEIF(APPLE)
+    SET(Luabind_LIBRARIES
+        optimized ${Luabind_ROOT}/build/src/libluabind.a
+        debug     ${Luabind_ROOT}/build/src/libluabind.a)
 ENDIF()
 
 set(Luabind_FOUND TRUE)
